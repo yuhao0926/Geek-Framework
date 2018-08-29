527bfeb6bf6e897d00c9e88530ce5806162979d6
yuhao <yuhao@218.68.250.117>
2018-08-29 10:26:16 +0800

    修改druid监控

--- src/main/java/com/geekcattle/core/shiro/ShiroConfiguration.java
+++ src/main/java/com/geekcattle/core/shiro/ShiroConfiguration.java
@@ -250,6 +250,7 @@ public class ShiroConfiguration {
 
         //<!-- 过滤链定义，从上向下顺序执行，一般将 /**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
         //<!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
+        filterChainDefinitionMap.put("/druid/**", "anon");
         filterChainDefinitionMap.put("/console/login", "anon");
         filterChainDefinitionMap.put("/console/logout", "logout");
         //配置记住我或认证通过可以访问的地址
